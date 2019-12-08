2bd90dcb1ff4a63d8b34b1e95ee24e2099fada5f
franko <francoastorga999@gmail.com>
2019-12-08 15:13:38 -0300

    Menu mejorado

--- Juego_naves.py
+++ Juego_naves.py
@@ -8,9 +8,9 @@ ventana_x=1024
 ventana_y=576
 j=0
 aleatorio=0
-tamaño_boton=(100,50)
+tamaño_boton=(200,40)
 espacio=0
-multi=0
+lista_rectangulos=[]
 
 
 #colores
@@ -20,10 +20,14 @@ amarillo=(255,255,0)
 naranja=(255,128,0)
 rojo=(254,0,0)
 verde=(57,255,20)
+gris=(125,127,125)
 
-def moverNave(x,y):
-	imagen_nave=pygame.image.load("imagenes/nave.png")
-	ventana.blit(imagen_nave,(x,y))
+
+#Jugando-------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------
+def MoverMouse(x,y):
+	imagen_mouse=pygame.image.load("imagenes/Apocalypse Cursor.png")
+	ventana.blit(imagen_mouse,(x,y))
 
 def Fondo():
 	pygame.mouse.set_visible(0)
@@ -46,60 +50,108 @@ def Meteoritos(x,y,demora):
 			j=j+1	
 		i=i+1
 
-
+#Botones------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------
 def Definir_espaciado(espaciado):
 	global espacio
 	espacio=tamaño_boton[1]+espaciado
 
 
-def Boton(tamaño,color,texto,tamFuente):
-	global espacio,multi
+def Boton(tamaño,color,texto,tamFuente,pos):
+	global espacio
 	centro=ventana_x/2
+
 	#Dibujo boton
-	aux=pygame.Rect(centro-tamaño[0]/2,ventana_y/2+espacio*multi,tamaño[0],tamaño[1])
+	aux=pygame.Rect(centro-tamaño[0]/2,ventana_y/2+espacio*pos,tamaño[0],tamaño[1])
 	pygame.draw.rect(ventana,color,aux)
 
 	#Texto en el Boton
-	fuente_letra=pygame.font.SysFont("Comic Sans",tamFuente)
+	fuente_letra=pygame.font.SysFont("Comic Sans MS",tamFuente)
 	texto=fuente_letra.render(texto,0,blanco)		
-	ventana.blit(texto,(centro-tamaño[0]/2+5,ventana_y/2+espacio*multi+10))
+	textoRect=texto.get_rect()
+	textoRect.center=(centro-tamaño[0]/2+(tamaño[0]/2),ventana_y/2+espacio*pos+(tamaño[1]/2))
+	ventana.blit(texto,textoRect)
+	
+	return aux
 
-	multi=multi+1
+def Cambio_color_boton(lista):
+	i=0
+	lista_aux=["Iniciar","Reglas","Salir"]
 
+	#Si el mouse colision con algun rectangulo , cambia de color
+	while i < len(lista):
+
+		if Hubo_Colision(lista[i])==True:
+			#Si cambia de color , vemos en que rectangulo esta para poenrle el texto
+			Boton(tamaño_boton,gris,lista_aux[i],20,i)	
+		else:
+			#Sino colisiona siguen igual
+			if i==0:
+				pygame.draw.rect(ventana,verde,lista[0])
+				Boton(tamaño_boton,verde,"Iniciar",20,0)	
+			if i==1:
+				pygame.draw.rect(ventana,naranja,lista[1])
+				Boton(tamaño_boton,naranja,"Reglas",20,1)
+			if i==2:	
+				pygame.draw.rect(ventana,rojo,lista[2])
+				Boton(tamaño_boton,rojo,"Salir",20,2)
+
+		
+		i=i+1
+			
+#-------------------------------------------------------------------------------------------------------------------
+#Logica#------------------------------------------------------------------------------------------------------------
+
+def Hubo_Colision(rectangulo):
+	bandera=False
+	rectangulo_colision=pygame.Rect(0,0,10,10)
+	rectangulo_colision.left,rectangulo_colision.top = pygame.mouse.get_pos()
+	if rectangulo_colision.colliderect(rectangulo):
+		bandera=True 
+
+	return bandera	
+
+
+
+
+#----------------------------------------------------------------------------------------------------------
+#Menu principal#------------------------------------------------------------------------------------------------------
 def Menu():
 	ventana.fill(negro)	
-	#Configuracion botones------------------------------------------------------------------------------------------------------------
-
+	#Configuracion botones
 	Definir_espaciado(10)
-
-	Boton(tamaño_boton,verde,"Iniciar",40)
-	Boton(tamaño_boton,naranja,"Reglas",40)
-	Boton(tamaño_boton,rojo,"Salir",40)
+	
+	lista_rectangulos.append(Boton(tamaño_boton,verde,"Iniciar",20,0))
+	lista_rectangulos.append(Boton(tamaño_boton,naranja,"Reglas",20,1))
+	lista_rectangulos.append(Boton(tamaño_boton,rojo,"Salir",20,2))
+	
 	#-----------------------------------------------------------------------------------------------------------------------------------
-
 	while True:
+
 		for evento in pygame.event.get():# recorro la lista de eventos que tiene pygame
 			if evento.type == QUIT: # si evento que ocurrio es del tipo QUIT 
 				pygame.quit() # detengo todos los módulos de pygame
 				sys.exit() # cerramos la ventana
+
+	#Colision----------------------------------------------------------------------------------------------
+		Cambio_color_boton(lista_rectangulos)
+
 		pygame.display.update()          
 
 
 
-
-
+#Inicio--------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------
 pygame.init()
 
 ventana = pygame.display.set_mode((ventana_x,ventana_y)) # creo un objeto ventana con sus medidas
 pygame.display.set_caption("Battle Rows") # pongo un titulo o mensaj
 
 
-
 while True: # bucle infinito
 
     #MOVIMIENTO NAVE---------------------------------------------
     posX,posY=pygame.mouse.get_pos() # tomo las coodenadas (x,y) del mouse 
-   
 
     #FUNCIONES--------------------------------------------------
     Menu()
@@ -112,10 +164,3 @@ while True: # bucle infinito
 
     pygame.display.update() # la ventana se va a estar actualizando
 
-
-
-    """
- fuente_letra=pygame.font.SysFont("Comic Sans",45)
-  textoAuxiliar=fuente_letra.render("Jugador 2, elija una opción",0,negro)
-  ventana.blit(textoAuxiliar,(230,240))
-  """
